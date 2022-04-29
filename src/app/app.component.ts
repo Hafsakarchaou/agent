b8b2160b10becb98eb4b0a33ea89a2275c085337
HafsaKarchaou <hafsadiy@gmail.com>
2022-04-29 22:44:14 +0000

    m1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { delay, filter } from 'rxjs/operators';
+
+import { NavigationEnd, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'matside';
-}
+  
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav; 
+  
+    constructor(private observer: BreakpointObserver) {}
+  
+    ngAfterViewInit() {
+      this.observer
+        .observe(['(max-width: 800px)'])
+        .subscribe((res) => {
+          if (res.matches) {
+            this.sidenav.mode = 'over';
+            this.sidenav.close();
+          } else {
+            this.sidenav.mode = 'side';
+            this.sidenav.open();
+          }
+        });
+      }  }
